5688c4dcaca576fe142352294a570c7071930b75
huangxiongbin <2810414811@qq.com>
2024-06-04 10:10:21 +0800

    成功对多项目重启，并刷选出springboot项目的启动

---
+++ build.bat
@@ -0,0 +1,2 @@
+./gradlew build -x buildSearchableOptions --stacktrace
+REM 完成后将./build/libs文件夹下的restart-1.0-SNAPSHOT.jar 然后再idea中settings 的plugin 通过disk加载插件
--- src/main/kotlin/com/hxb/restart/ProjectStarterService.kt
+++ src/main/kotlin/com/hxb/restart/ProjectStarterService.kt
@@ -1,24 +1,27 @@
 package com.hxb.restart
 
-import com.intellij.execution.ExecutionException
-import com.intellij.execution.ExecutionManager
-import com.intellij.execution.ExecutorRegistry
-import com.intellij.execution.RunManager
+import com.fasterxml.jackson.jr.ob.JSON
+import com.intellij.execution.*
+import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ApplicationComponent
-import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.wm.ToolWindowId
+import com.intellij.psi.PsiManager
+import com.intellij.psi.xml.XmlFile
+import net.minidev.json.JSONObject
+import java.io.File
 import java.net.DatagramPacket
 import java.net.InetAddress
 import java.net.MulticastSocket
 import java.net.ServerSocket
 import java.util.function.Consumer
-
 
 class ProjectStarterService : ApplicationComponent {
     private var serverSocket: ServerSocket? = null
@@ -44,6 +47,7 @@ class ProjectStarterService : ApplicationComponent {
                     logger.info("ProjectStarterService Plugin is running 3.")
                     val message = String(packet.data, 0, packet.length).trim()
                     println("Received message: $message")
+
                     ApplicationManager.getApplication().invokeLater {
                         restartSpringBootProject(message)
                     }
@@ -67,6 +71,60 @@ class ProjectStarterService : ApplicationComponent {
         }
     }
 
+
+    fun getMainClass(project: Project): @NlsSafe String? {
+
+        val psiManager = PsiManager.getInstance(project)
+        val baseDir = project.baseDir
+        val pomFile = baseDir.findChild("pom.xml")
+        println("Received pomFile: $pomFile")
+        if (pomFile != null) {
+            val psiFile = psiManager.findFile(pomFile)
+            if (psiFile is XmlFile) {
+                println("Received pomFile: 1")
+                val rootTag = psiFile.rootTag
+                val buildTag = rootTag?.findFirstSubTag("build")
+                val pluginsTag = buildTag?.findFirstSubTag("plugins")
+                val pluginTags = pluginsTag?.findSubTags("plugin")
+                println("Received pomFile size: ${pluginTags!!.size}")
+                pluginTags?.forEach { pluginTag ->
+                    println("Received pomFile: $pluginTag")
+                    val configurationTag = pluginTag.findFirstSubTag("configuration")
+                    val mainClassTag = configurationTag?.findFirstSubTag("mainClass")
+                    val mainClass = mainClassTag?.value?.text
+                    if(mainClass != null){
+                        return mainClass
+                    }
+                }
+            }
+        }
+        return null
+
+    }
+
+    fun getRunConfigurationWithMainClass(project: Project,): RunConfiguration? {
+        val mainClass = getMainClass(project) ?: return null
+        val runManager = RunManager.getInstance(project)
+        val allConfigurations = runManager.allConfigurationsList
+        logger.info(mainClass)
+        allConfigurations.forEach { configuration ->
+            logger.info( JSON.std.asString(configuration))
+
+        }
+        return null
+    }
+
+    fun findConfig(openProjects:Project): RunnerAndConfigurationSettings? {
+        val runManager = RunManager.getInstance(openProjects)
+        val configurations = runManager.allSettings
+
+       return configurations.filter { it ->
+           logger.warn("it.configuration.javaClass.name:"+it.configuration.javaClass.name)
+           it.configuration.javaClass.name == "com.intellij.spring.boot.run.SpringBootApplicationRunConfiguration"
+
+       }.firstOrNull()
+    }
+
     @Throws(ExecutionException::class)
     private fun restartSpringBootProject(message: String) {
         val projectName = message.split(" ")[1].trim()
@@ -80,16 +138,17 @@ class ProjectStarterService : ApplicationComponent {
         if ("restart" != command.trim()) {
             return
         }
+
+        println(getMainClass(openProjects))
         val runManager = RunManager.getInstance(openProjects)
-        val configuration = runManager.selectedConfiguration
+
+        val configuration = /* runManager.selectedConfiguration*/ findConfig(openProjects)
         if (configuration != null) {
             logger.info("ProjectStarterService Plugin is running cf name.," + configuration!!.name)
-            val runConfiguration = configuration.configuration
+            val runConfiguration = /*configuration.configuration*/ configuration.configuration
             val executor = ExecutorRegistry.getInstance().getExecutorById(ToolWindowId.RUN)
             val runner = ProgramRunner.getRunner(ToolWindowId.DEBUG, runConfiguration)
             if (runner != null) {
-                // Stop the current running configuration
-                // Stop the current running configuration
                 ExecutionManager.getInstance(openProjects)
                     .getContentManager().allDescriptors.forEach(Consumer<RunContentDescriptor> { descriptor: RunContentDescriptor ->
                         if (descriptor.processHandler!!.isProcessTerminating || descriptor.processHandler!!
