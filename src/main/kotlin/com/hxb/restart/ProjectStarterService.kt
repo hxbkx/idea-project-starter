93ee50e7817e49e4ab6f55b800bb69c9144cfb0a
huangxiongbin <2810414811@qq.com>
2024-06-17 16:54:26 +0800

    clear

--- src/main/kotlin/com/hxb/restart/ProjectStarterService.kt
+++ src/main/kotlin/com/hxb/restart/ProjectStarterService.kt
@@ -1,8 +1,6 @@
 package com.hxb.restart
 
-import com.fasterxml.jackson.jr.ob.JSON
 import com.intellij.execution.*
-import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.ProgramRunner
 import com.intellij.execution.ui.RunContentDescriptor
@@ -10,13 +8,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.roots.ProjectFileIndex
-import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.wm.ToolWindowId
-import com.intellij.psi.PsiManager
-import com.intellij.psi.xml.XmlFile
-import net.minidev.json.JSONObject
-import java.io.File
 import java.net.DatagramPacket
 import java.net.InetAddress
 import java.net.MulticastSocket
@@ -28,11 +20,11 @@ class ProjectStarterService : ApplicationComponent {
     val logger = com.intellij.openapi.diagnostic.Logger.getInstance(ProjectStarterService::class.java)
 
     override fun initComponent() {
-        logger.info("ProjectStarterService Plugin is running.")
+        logger.info("project starter service Plugin is running.")
 
         Thread {
 
-            logger.info("ProjectStarterService Plugin is running 1.")
+            logger.info("project starter service Plugin is running 1.")
             val multicastGroupAddress = "230.0.0.0"
             val multicastPort = 43211
             val group = InetAddress.getByName(multicastGroupAddress)
@@ -40,11 +32,11 @@ class ProjectStarterService : ApplicationComponent {
             multicastSocket.joinGroup(group)
             while (true) {
                 try {
-                    logger.info("ProjectStarterService Plugin is running 2.")
+                    logger.info("project starter service Plugin is running 2.")
                     val buffer = ByteArray(1000)
                     val packet = DatagramPacket(buffer, buffer.size)
                     multicastSocket.receive(packet)
-                    logger.info("ProjectStarterService Plugin is running 3.")
+                    logger.info("project starter service Plugin is running 3.")
                     val message = String(packet.data, 0, packet.length).trim()
                     println("Received message: $message")
 
@@ -54,7 +46,7 @@ class ProjectStarterService : ApplicationComponent {
 
 
                 } catch (e: Exception) {
-                    logger.error("ProjectStarterService Plugin is error 4.", e)
+                    logger.error("project starter service Plugin is error 4.", e)
                 }
             }
 
@@ -72,7 +64,7 @@ class ProjectStarterService : ApplicationComponent {
     }
 
 
-    fun getMainClass(project: Project): @NlsSafe String? {
+    /*fun getMainClass(project: Project): @NlsSafe String? {
 
         val psiManager = PsiManager.getInstance(project)
         val baseDir = project.baseDir
@@ -92,7 +84,7 @@ class ProjectStarterService : ApplicationComponent {
                     val configurationTag = pluginTag.findFirstSubTag("configuration")
                     val mainClassTag = configurationTag?.findFirstSubTag("mainClass")
                     val mainClass = mainClassTag?.value?.text
-                    if(mainClass != null){
+                    if (mainClass != null) {
                         return mainClass
                     }
                 }
@@ -100,29 +92,18 @@ class ProjectStarterService : ApplicationComponent {
         }
         return null
 
-    }
+    }*/
 
-    fun getRunConfigurationWithMainClass(project: Project,): RunConfiguration? {
-        val mainClass = getMainClass(project) ?: return null
-        val runManager = RunManager.getInstance(project)
-        val allConfigurations = runManager.allConfigurationsList
-        logger.info(mainClass)
-        allConfigurations.forEach { configuration ->
-            logger.info( JSON.std.asString(configuration))
 
-        }
-        return null
-    }
-
-    fun findConfig(openProjects:Project): RunnerAndConfigurationSettings? {
+    fun findConfig(openProjects: Project): RunnerAndConfigurationSettings? {
         val runManager = RunManager.getInstance(openProjects)
         val configurations = runManager.allSettings
 
-       return configurations.filter { it ->
-           logger.warn("it.configuration.javaClass.name:"+it.configuration.javaClass.name)
-           it.configuration.javaClass.name == "com.intellij.spring.boot.run.SpringBootApplicationRunConfiguration"
+        return configurations.filter { it ->
+            logger.warn("it.configuration.javaClass.name:" + it.configuration.javaClass.name)
+            it.configuration.javaClass.name == "com.intellij.spring.boot.run.SpringBootApplicationRunConfiguration"
 
-       }.firstOrNull()
+        }.firstOrNull()
     }
 
     @Throws(ExecutionException::class)
@@ -131,21 +112,16 @@ class ProjectStarterService : ApplicationComponent {
         val command = message.split(" ")[0].trim()
         val openProjects = ProjectManager.getInstance().openProjects.filter { it.name == projectName }.firstOrNull()
         if (openProjects == null) {
-            logger.warn("ProjectStarterService Plugin not find project return" + projectName)
+            logger.warn("project starter service Plugin not find project return" + projectName)
             return;
         }
-        var appName = openProjects.name
         if ("restart" != command.trim()) {
             return
         }
-
-        println(getMainClass(openProjects))
-        val runManager = RunManager.getInstance(openProjects)
-
-        val configuration = /* runManager.selectedConfiguration*/ findConfig(openProjects)
+        val configuration = findConfig(openProjects)
         if (configuration != null) {
-            logger.info("ProjectStarterService Plugin is running cf name.," + configuration!!.name)
-            val runConfiguration = /*configuration.configuration*/ configuration.configuration
+            logger.info("project starter service Plugin is running cf name.," + configuration!!.name)
+            val runConfiguration = configuration.configuration
             val executor = ExecutorRegistry.getInstance().getExecutorById(ToolWindowId.DEBUG)
             val runner = ProgramRunner.getRunner(ToolWindowId.DEBUG, runConfiguration)
             if (runner != null) {
@@ -158,10 +134,10 @@ class ProjectStarterService : ApplicationComponent {
                         }
                         descriptor.processHandler!!.destroyProcess()
                     })
-                logger.info("ProjectStarterService Plugin is running runner name.," + runner!!.runnerId)
+                logger.info("project starter service Plugin is running runner name.," + runner!!.runnerId)
                 val environment = executor?.let { ExecutionEnvironment(it, runner, configuration, openProjects) }
                 if (environment != null) {
-                    logger.info("ProjectStarterService Plugin is running execute.,")
+                    logger.info("project starter service Plugin is running execute.,")
                     runner.execute(environment).run { }
                 }
             }
